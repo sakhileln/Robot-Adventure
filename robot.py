74f63b7ffecb41b9fda8766d060f8b971657c050
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-11-24 17:18:36 +0200

    Add get_position method.

--- robot.py
+++ robot.py
@@ -15,6 +15,21 @@ class RobotBase(ABC):
 
 class Robot(RobotBase):
     def __init__(self, name: str, position: tuple = (0, 0), facing: int = 0):
+        """
+        Initializes a new robot with a given name, position, and facing direction.
+
+        :param name: The name of the robot.
+        :param position: The initial position of the robot as (x, y).
+        :param facing: The direction the robot is facing in degrees (0-360).
+        """
         self.name = name
         self.__position = position
         self.__facing = facing
+
+    def get_position(self):
+        return self.__position
+
+    def move_forward(self, distance=1): ...
+    def turn_left(self, degrees): ...
+    def turn_right(self, degrees): ...
+    def talk(self, message): ...
