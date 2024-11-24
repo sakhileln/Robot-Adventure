1f0961887b70b49573571700b51c920ea9190645
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-11-24 16:45:22 +0200

    Abstract RobotBase base class.

---
+++ robot.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+
+
+# Abstract Base Class
+class RobotBase(ABC):
+    @abstractmethod
+    def move_forward(self, distance=1): ...
+    @abstractmethod
+    def turn_left(self, degrees): ...
+    @abstractmethod
+    def turn_right(self, degrees): ...
+    @abstractmethod
+    def talk(self, message): ...
