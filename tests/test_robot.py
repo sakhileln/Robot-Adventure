66b83e22566f95cb8c6021a5b8cb0e5eee8951f0
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-11-24 17:27:11 +0200

    Test robot moving forward.

--- tests/test_robot.py
+++ tests/test_robot.py
@@ -11,6 +11,16 @@ class TestRobot(unittest.TestCase):
             robot._Robot__facing, 90
         )
 
+    def test_move_forward(self):
+        """Test Robot moving forward."""
+        robot = Robot(name="CP3O", position=(0, 0), facing=0)
+        robot.move_forward(5)
+        self.assertEqual(
+            robot.get_position(), (5, 0)
+        )
+        self.assertEqual(
+            robot._Robot__facing, 0
+        )
 
 if __name__ == "__main__":
     unittest.main()
