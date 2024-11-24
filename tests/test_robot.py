d16a008cae4e776c72a745cda0d37fd1948133ba
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-11-24 17:05:41 +0200

    Test robot creation and initialization.

--- tests/test_robot.py
+++ tests/test_robot.py
@@ -1,7 +1,15 @@
 import unittest
+from robot import Robot
 
-
-class TestRobot(unittest.TestCase): ...
+class TestRobot(unittest.TestCase):
+    def test_robot_initialization(self):
+        """Test the creation and initialization of a Robot."""
+        robot = Robot(name="CP3O", position=(0, 0), facing=90)
+        self.assertEqual(robot.name, "CP3O")
+        self.assertEqual(robot.get_position(), (0, 0))
+        self.assertEqual(
+            robot._Robot__facing, 90
+        )
 
 
 if __name__ == "__main__":
