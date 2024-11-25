19939882a7532e96c64f2eb618434ab1bfb05baa
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-11-25 20:33:24 +0200

    Test class methods and static methods.

--- main.py
+++ main.py
@@ -9,6 +9,23 @@ def main():
 
     # Test methods
     print(robot1)
+    robot1.move_forward(5)
+    robot1.turn_right(90)
+    robot1.talk("I'm a basic robot.")
+
+    print(racing_robot)
+    racing_robot.move_forward(3)
+
+    print(battle_bot)
+    battle_bot.attack()
+    battle_bot.talk("Prepare to fight!")
+
+    # Test class methods and static methods
+    default_robot = Robot.create_default_robot()
+    print(f"Created {default_robot}")
+
+    distance = Robot.calculate_distance(robot1, battle_bot)
+    print(f"Distance between Robo1 and CombatBot: {distance:.2f} meters.")
 
 
 if __name__ == "__main__":
