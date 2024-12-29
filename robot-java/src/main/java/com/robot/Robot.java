c3c0b9226b45e555a39fd59e5262398b5816b742
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-12-29 10:22:02 +0200

    Use 'Point2D.Double' to store and retrieve robot position.

--- robot-java/src/main/java/com/robot/Robot.java
+++ robot-java/src/main/java/com/robot/Robot.java
@@ -2,14 +2,15 @@ package com.robot;
 
 import com.robot.RobotBase;
 import java.lang.Math;
+import java.awt.geom.Point2D;
 
 class Robot extends RobotBase {
     // Fields
     String robotName;
-    private double robotPosition;
+    private Point2D.Double robotPosition = new Point2D.Double(0, 0);
     private int robotFacing;
     // Constructor
-    public Robot(String name, double position, int facing) {
+    public Robot(String name, Point2D.Double position, int facing) {
         robotName = name;
         robotPosition = position;
         robotFacing = facing;
@@ -20,8 +21,14 @@ class Robot extends RobotBase {
         double dx = distance * Math.sin(angle_rad);
         double dy = distance * Math.cos(angle_rad);
         // Extract (x,y) from position
+        double x = robotPosition.getX();
+        double y = robotPosition.getY();
         // Update position with new (x + dx, y + dy)
-        System.out.println(robotName + " moved forward by " + distance + " meters to " + "update position");
+        robotPosition.setLocation(x + dx, y + dy);
+        System.out.print(robotName + " moved forward by " + distance + " meters to ");
+        System.out.println("(" + robotPosition.getX() + "," + robotPosition.getY() + ")");
     }
+
+
 
 }
