541bafaeea0fc9dbe3045cc974ed9872760073c8
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-12-29 11:35:21 +0200

    Inherit constructor from parent and give it required fields.

--- robot-java/src/main/java/com/robot/RacingRobot.java
+++ robot-java/src/main/java/com/robot/RacingRobot.java
@@ -1,4 +1,10 @@
 package com.robot;
 
-public class RacingRobot {
+import java.awt.geom.Point2D;
+
+public class RacingRobot extends Robot {
+
+    public RacingRobot(String name, Point2D.Double position, double facing) {
+        super(name, position, facing);
+    }
 }
