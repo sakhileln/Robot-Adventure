309b7b03a6f4a8fb7756cfbea35e15429e88fc6a
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-12-29 10:24:32 +0200

    Fix spelling for 'move_forward'.

--- robot-java/src/main/java/com/robot/RobotBase.java
+++ robot-java/src/main/java/com/robot/RobotBase.java
@@ -1,7 +1,7 @@
 package com.robot;
 
 public abstract class RobotBase {
-    abstract void move_forwad(double distance);
+    abstract void move_forward(double distance);
     abstract void turn_left(double degrees);
     abstract void turn_right(double degrees);
     abstract String talk(String message);
