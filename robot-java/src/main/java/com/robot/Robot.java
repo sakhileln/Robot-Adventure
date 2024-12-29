5dc04ab5f4194ee62766c650c17753452d701bf8
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-12-29 10:01:27 +0200

    Implement 'move_forward' abstract method

--- robot-java/src/main/java/com/robot/Robot.java
+++ robot-java/src/main/java/com/robot/Robot.java
@@ -1,10 +1,27 @@
 package com.robot;
 
-public class Robot {
-    public abstract class RobotBase {
-        abstract void move_forward(int distance);
-        abstract void turn_left(int degrees);
-        abstract void turn_right(int degrees);
-        abstract String talk(String message);
+import com.robot.RobotBase;
+import java.lang.Math;
+
+class Robot extends RobotBase {
+    // Fields
+    String robotName;
+    private double robotPosition;
+    private int robotFacing;
+    // Constructor
+    public Robot(String name, double position, int facing) {
+        robotName = name;
+        robotPosition = position;
+        robotFacing = facing;
     }
+    // Methods
+    void move_forward(double distance) {
+        double angle_rad = Math.toRadians(robotFacing);
+        double dx = distance * Math.sin(angle_rad);
+        double dy = distance * Math.cos(angle_rad);
+        // Extract (x,y) from position
+        // Update position with new (x + dx, y + dy)
+        System.out.println(robotName + " moved forward by " + distance + " meters to " + "update position");
+    }
+
 }
