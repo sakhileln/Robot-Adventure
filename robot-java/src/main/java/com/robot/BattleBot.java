605bdfbf0594997e12ae5598921fa6e15994f12c
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-12-29 11:49:37 +0200

    Override 'talk' method.

--- robot-java/src/main/java/com/robot/BattleBot.java
+++ robot-java/src/main/java/com/robot/BattleBot.java
@@ -1,4 +1,13 @@
 package com.robot;
 
-public class BattleBot {
+import java.awt.geom.Point2D;
+
+public class BattleBot extends Robot {
+    public BattleBot(String name, Point2D.Double position, double facing) {
+        super(name, position, facing);
+    }
+
+    void attack() { System.out.println(robotName + " is attacking with laser beams!"); }
+    @Override
+    void talk(String message) { System.out.println("BattleBot " + robotName + " says: " + message); }
 }
