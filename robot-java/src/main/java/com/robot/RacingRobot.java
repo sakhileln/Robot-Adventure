66e3e7d779726478a46f2276bebc32126d0f0305
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-12-29 11:41:24 +0200

    Override 'move_forward' method to move twice as fast.

--- robot-java/src/main/java/com/robot/RacingRobot.java
+++ robot-java/src/main/java/com/robot/RacingRobot.java
@@ -3,8 +3,13 @@ package com.robot;
 import java.awt.geom.Point2D;
 
 public class RacingRobot extends Robot {
-
+    // Inherit constructor from parent
     public RacingRobot(String name, Point2D.Double position, double facing) {
         super(name, position, facing);
+    }
+    @Override
+    void move_forward(double distance) {
+        /*Override move_forward method to make robot move twice as fast.*/
+        super.move_forward(2 * distance);
     }
 }
